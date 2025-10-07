1285fea3bba355b6762adc77505bd6bf73057938
hewenyu <he875027547@126.com>
2025-10-08 00:43:19 +0800

    基于setter方法注入

---
+++ spring-02-ioc-xml/src/main/java/com/example/spring/di/Book.java
@@ -0,0 +1,12 @@
+package com.example.spring.di;
+
+import lombok.Data;
+
+@Data
+public class Book {
+
+    private String name;
+
+    private String author;
+
+}
---
+++ spring-02-ioc-xml/src/test/java/com/example/spring/test/BookTest.java
@@ -0,0 +1,16 @@
+package com.example.spring.test;
+
+import com.example.spring.di.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BookTest {
+
+    @Test
+    public void testGetBookObj(){
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean-di.xml");
+        Book bean = applicationContext.getBean(Book.class);
+        System.out.println(bean);
+    }
+
+}
