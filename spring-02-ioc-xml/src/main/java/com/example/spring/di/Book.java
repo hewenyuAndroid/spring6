cf74735a3a21fbbb20e0a3f72fe089d09e53eb61
hewenyu <he875027547@126.com>
2025-10-12 23:31:35 +0800

    构造器注入

--- spring-02-ioc-xml/src/main/java/com/example/spring/di/Book.java
+++ spring-02-ioc-xml/src/main/java/com/example/spring/di/Book.java
@@ -5,6 +5,16 @@ import lombok.Data;
 @Data
 public class Book {
 
+    public Book() {
+        System.out.println("Book constructor 无参构造...");
+    }
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+        System.out.println("Book constructor 有参构造...");
+    }
+
     private String name;
 
     private String author;
--- spring-02-ioc-xml/src/test/java/com/example/spring/test/BookTest.java
+++ spring-02-ioc-xml/src/test/java/com/example/spring/test/BookTest.java
@@ -7,10 +7,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class BookTest {
 
     @Test
-    public void testGetBookObj(){
+    public void testGetBookObj() {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean-di.xml");
-        Book bean = applicationContext.getBean(Book.class);
+//        Book bean = applicationContext.getBean(Book.class);
+        Book bean = applicationContext.getBean("book", Book.class);
         System.out.println(bean);
     }
+
+    @Test
+    public void testGetBookObjCons() {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean-di.xml");
+        Book bean = applicationContext.getBean("book1", Book.class);
+        System.out.println(bean);
+    }
+
 
 }
